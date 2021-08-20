ad5a309dc59653c456324fd1aaec8e664d0dbb09
Adam Eshel <adameshel@Adams-MacBook-Pro.local>
2021-08-20 10:39:24 +0200

    fft part merged

--- Autocorr/autocorr_cmls.py
+++ Autocorr/autocorr_cmls.py
@@ -17,7 +17,7 @@ def excludenans(arr):
     return np.compress(bool_data,arr)
 
 class Autocorr():
-    def __init__(self, df, bw, cutoff_distance_km=90.0):
+    def __init__(self, df, bw=1.0, cutoff_distance_km=90.0):
         """
         df columns can be -
             Link_num, L, x, y, z
@@ -43,79 +43,92 @@ class Autocorr():
         bw - Bandwidth in km.
         cutoff_distance_km - The maximal distance between measurement points accounted for.
         """
-
-        bw = bw * 1e3 # convert to meters
-        cutoff_distance_km = cutoff_distance_km * 1e3 # convert to meters
-        if 'L' in df:
-            xMax = np.max([df.xa.max(), df.xb.max()])
-            xMin = np.min([df.xa.min(), df.xb.min()])
-            yMax = np.max([df.ya.max(), df.yb.max()])
-            yMin = np.min([df.ya.min(), df.yb.min()])
-        else:
-            xMax = np.max(df.x.max())
-            xMin = np.min(df.x.min())
-            yMax = np.max(df.y.max())
-            yMin = np.min(df.y.min())
-            
-        p_prep = np.array( df[['x','y','z']] )
-        max_num_of_rg_in_row = len(p_prep[0,2])
-        ## A loop for defining the VRGs per link where the number  
-        ## of VRGs per link is not the same
-        # if type(p_prep[0,2]) is tuple:
-        #     for row in range(len(p_prep[:,0])):
-        #         num_of_rg_in_row = len(p_prep[row,0])
-        #         p_prep[row,2] = p_prep[row,2][:num_of_rg_in_row]
-        # else:
-        #     for row in range(len(p_prep[:,0])):
-        #         num_of_rg_in_row = 1
-        #         p_prep[row,2] = p_prep[row,2]
-
-        for row in range(len(p_prep[:,0])):
-            num_of_rg_in_row = len(p_prep[row,0])
-            p_prep[row,2] = p_prep[row,2][:num_of_rg_in_row]
+        if isinstance(df, pd.DataFrame): # df is a Dataframe
+            self.WKT = False
+            bw = bw * 1e3 # convert to meters
+            cutoff_distance_km = cutoff_distance_km * 1e3 # convert to meters
+            if 'L' in df:
+                xMax = np.max([df.xa.max(), df.xb.max()])
+                xMin = np.min([df.xa.min(), df.xb.min()])
+                yMax = np.max([df.ya.max(), df.yb.max()])
+                yMin = np.min([df.ya.min(), df.yb.min()])
+            else:
+                xMax = np.max(df.x.max())
+                xMin = np.min(df.x.min())
+                yMax = np.max(df.y.max())
+                yMin = np.min(df.y.min())
                 
-            
-        p = np.zeros([len(p_prep[:,0]) * max_num_of_rg_in_row,
-                    len(p_prep[0,:])])
-        link_num_prep = []
-        link_L_prep = []
-        p_row = 0
-        element = 0
-        for row in range(len(p_prep[:,0])):
-            if row != 0:
-                p_row = p_row + element +1
-            for col in range(len(p_prep[0,:])):
-                for element in range(len(p_prep[row,2])):
-                    # import pdb; pdb.set_trace()
-                    p[p_row+element,col] = p_prep[row,col][element]
-        for cut_point in range(len(p[:,0])): 
-            if p[cut_point,0] == 0: 
-                break
-        if len(p[:,0]) == (cut_point + 1):
-            p_filtered = p
-        else:
-            p_filtered = np.delete(p, 
-                                range(cut_point,len(p_prep[:,0]) *\
-                                        max_num_of_rg_in_row), 
-                                0)
-        if 'L' in df:
+            p_prep = np.array( df[['x','y','z']] )
+            max_num_of_rg_in_row = len(p_prep[0,2])
+            ## A loop for defining the VRGs per link where the number  
+            ## of VRGs per link is not the same
+            # if type(p_prep[0,2]) is tuple:
+            #     for row in range(len(p_prep[:,0])):
+            #         num_of_rg_in_row = len(p_prep[row,0])
+            #         p_prep[row,2] = p_prep[row,2][:num_of_rg_in_row]
+            # else:
+            #     for row in range(len(p_prep[:,0])):
+            #         num_of_rg_in_row = 1
+            #         p_prep[row,2] = p_prep[row,2]
+
             for row in range(len(p_prep[:,0])):
-                for element in range(len(p_prep[row,2])):
-                    link_num_prep.append(df['Link_num'][row])
-                    link_L_prep.append(df['L'][row])
+                num_of_rg_in_row = len(p_prep[row,0])
+                p_prep[row,2] = p_prep[row,2][:num_of_rg_in_row]
+                    
+                
+            p = np.zeros([len(p_prep[:,0]) * max_num_of_rg_in_row,
+                        len(p_prep[0,:])])
+            link_num_prep = []
+            link_L_prep = []
+            p_row = 0
+            element = 0
+            for row in range(len(p_prep[:,0])):
+                if row != 0:
+                    p_row = p_row + element +1
+                for col in range(len(p_prep[0,:])):
+                    for element in range(len(p_prep[row,2])):
+                        # import pdb; pdb.set_trace()
+                        p[p_row+element,col] = p_prep[row,col][element]
+            for cut_point in range(len(p[:,0])): 
+                if p[cut_point,0] == 0: 
+                    break
+            if len(p[:,0]) == (cut_point + 1):
+                p_filtered = p
+            else:
+                p_filtered = np.delete(p, 
+                                    range(cut_point,len(p_prep[:,0]) *\
+                                            max_num_of_rg_in_row), 
+                                    0)
+            if 'L' in df:
+                for row in range(len(p_prep[:,0])):
+                    for element in range(len(p_prep[row,2])):
+                        link_num_prep.append(df['Link_num'][row])
+                        link_L_prep.append(df['L'][row])
 
-        self.df_p = pd.DataFrame(p_filtered, columns=['x','y','z'])
-        if 'L' in self.df_p:
-            self.df_p['l_num'] = link_num_prep
-            self.df_p['L'] = link_L_prep
+            self.df_p = pd.DataFrame(p_filtered, columns=['x','y','z'])
+            if 'L' in self.df_p:
+                self.df_p['l_num'] = link_num_prep
+                self.df_p['L'] = link_L_prep
 
-        self.hs = np.arange(bw, 
-                    np.min([np.max([xMax-xMin,yMax-yMin]), 
-                    cutoff_distance_km]), 
-                    bw*2.0)
-        p = self.df_p[['x','y','z']].values
-        self.distances = squareform( pdist( p[:,:2] ) )
-        self.ac = self._AC( p, self.hs, bw )
+            self.hs = np.arange(bw, 
+                        np.min([np.max([xMax-xMin,yMax-yMin]), 
+                        cutoff_distance_km]), 
+                        bw*2.0)
+            p = self.df_p[['x','y','z']].values
+            self.distances = squareform( pdist( p[:,:2] ) )
+            self.ac = self._AC( p, self.hs, bw )
+        else: # df is gridded data which will undergo FFT. bw is now a gridpoint length.
+            self.res = bw
+            self.Z = df
+            lenx = np.shape(df)[1]
+            x = np.linspace(-(lenx/2.0)*self.res,(lenx/2.0)*self.res,lenx)
+            leny = np.shape(df)[0]
+            y = np.linspace(-(leny/2.0)*self.res,(leny/2.0)*self.res,leny)
+            self.X, self.Y = np.meshgrid(x,y)
+            fft = np.fft.fftshift((np.fft.fft2(self.Z))) / len(self.Z.ravel())
+            self.s = abs(np.fft.fftshift(np.fft.ifft2(fft*np.conjugate(fft))))
+            self.s = self.s / np.nanmax(self.s)
+            self.WKT = True # Wiener-Khintchine theorem
 
 
     def __call__(self, optimize=True):
@@ -127,46 +140,62 @@ class Autocorr():
         optimize=False: beta_L is the max value and alpha_L is the 
         value of h at which the function lost 95% (not recommended).
         '''
-        ## Choose the nugget before optimizing
-        if len(self.hs) > 15:
-            mins = int(len(self.ac[1])/6)
-            self.nugget = np.nanmedian(np.sort(self.ac[1])[:mins])
-        else:
-            self.nugget = np.min(self.ac[1])
+        if self.WKT == False:
+            ## Choose the nugget before optimizing
+            if len(self.hs) > 15:
+                mins = int(len(self.ac[1])/6)
+                self.nugget = np.nanmedian(np.sort(self.ac[1])[:mins])
+            else:
+                self.nugget = np.min(self.ac[1])
 
-        if optimize==True:
-            self.magnitude_beta = 10 ** (int(np.log10(np.var(self.ac[1]))))
-            self.magnitude_alpha = 10 ** (int(np.log10(np.nanmean(self.hs))))
-            self.ac[0] = self.ac[0] / self.magnitude_alpha
-            self.ac[1] = self.ac[1] / self.magnitude_beta
-            self.nugget = self.nugget / self.magnitude_beta
-            try:
-                popt, _ = curve_fit(
-                    f=acf_original, 
-                    xdata=self.ac[0], 
-                    ydata=self.ac[1]-self.nugget,
-                    p0=[1,1,1],
-                    bounds=[0,(1e4,np.inf,7)]
-                )
-                self.alpha_L, self.beta_L, self.gamma_L = popt
-                self.alpha_L  = self.alpha_L * self.magnitude_alpha
-                self.beta_L = self.beta_L * self.magnitude_beta
-                self.ac[0] = self.ac[0] * self.magnitude_alpha
-                self.ac[1] = self.ac[1] * self.magnitude_beta
-                self.nugget = self.nugget * self.magnitude_beta
-            except:
-                self.alpha_L = np.nan
-                self.beta_L = np.nan
-                self.gamma_L = np.nan
-                self.nugget = np.nan
+            if optimize==True:
+                self.magnitude_beta = 10 ** (int(np.log10(np.var(self.ac[1]))))
+                self.magnitude_alpha = 10 ** (int(np.log10(np.nanmean(self.hs))))
+                self.ac[0] = self.ac[0] / self.magnitude_alpha
+                self.ac[1] = self.ac[1] / self.magnitude_beta
+                self.nugget = self.nugget / self.magnitude_beta
+                try:
+                    popt, pcov = curve_fit(
+                        f=acf_original, 
+                        xdata=self.ac[0], 
+                        ydata=self.ac[1]-self.nugget,
+                        p0=[1,1,1],
+                        bounds=[0,(1e4,np.inf,7)]
+                    )
+                    self.std_error = np.sqrt(np.diag(pcov))
+                    self.alpha_L, self.beta_L, self.gamma_L = popt
+                    self.alpha_L  = self.alpha_L * self.magnitude_alpha
+                    self.beta_L = self.beta_L * self.magnitude_beta
+                    self.ac[0] = self.ac[0] * self.magnitude_alpha
+                    self.ac[1] = self.ac[1] * self.magnitude_beta
+                    self.nugget = self.nugget * self.magnitude_beta
+                except:
+                    self.alpha_L = np.nan
+                    self.beta_L = np.nan
+                    self.gamma_L = np.nan
+                    self.nugget = np.nan
+            else:
+                # Max value of ACF
+                self.beta_L = self.ac[1][0]
+                # Correlation distance as h where 
+                #the value of ACF decreases by 95%
+                self.alpha_L = \
+                    self.hs[np.sum((self.ac[1] - self.nugget) >= \
+                        (np.nanmax(self.ac[1] - self.nugget) * 0.05)) -1]
         else:
-            # Max value of ACF
-            self.beta_L = self.ac[1][0]
-            # Correlation distance as h where 
-            #the value of ACF decreases by 95%
-            self.alpha_L = \
-                self.hs[np.sum((self.ac[1] - self.nugget) >= \
-                    (np.nanmax(self.ac[1] - self.nugget) * 0.05)) -1]
+            xdata = np.vstack((self.X.ravel(), self.Y.ravel()))
+            self.pars, pcov = curve_fit(
+                f=acf_original_2d,
+                xdata=xdata,
+                ydata=self.s.ravel(),
+                p0=[10*self.res,1,1]
+            )   
+            self.std_error = np.sqrt(np.diag(pcov))
+
+            self.ac_2d = np.reshape(
+                acf_original_2d(xdata, self.pars[0], self.pars[1], self.pars[2]),
+                np.shape(self.X)
+                )
         
     def _ACh( self, P, h, bw ):
         '''
